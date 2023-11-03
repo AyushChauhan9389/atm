63dff70314174d957a070d90d4c19aeff4379fab
Clust <ayush.chuahan.22cse@bmu.edu.in>
2023-11-03 12:22:25 +0530

    first

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+
+
+import atm.Login;
+
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException, SQLException {
+        Login login = new Login();
+        login.loginView();
+    }
+}
